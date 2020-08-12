c866a10db9fa809cfc9c3a2b9b8feafb93c6e672
Emilio Vázquez <emiliovazquez@Emilios-MacBook-Air.local>
2020-08-12 17:22:01 -0500

    added view for employee

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,25 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function App() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  }; 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p={2} >
+      <Button color = 'primary' variant="contained" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        <MenuIcon />
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+      <Box p={2} >
+        <Typography variant="h3" gutterBottom>
+          Hi Emilio!
+        </Typography>
+      </Box>
+      <Box p={1} >
+        <Button color = 'primary' variant="contained" fullWidth> Add Transaction </Button>
+      </Box>
+      <Box p={1} >
+        <Button color = 'primary' variant="contained" fullWidth> Add Client </Button>
+      </Box>
+      <Box p={2} >
+        <Typography variant="h5" gutterBottom>
+          Past transactions
+        </Typography>
+      </Box>
+      <List component="nav" aria-label="secondary mailbox folders">
+        <ListItem >
+          <ListItemText primary="12:01 pm Client: None Items: 2water" />
+          <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+        </ListItemSecondaryAction>
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="1:12 pm Client: Martha Items: 1water"  />
+          <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+        </ListItemSecondaryAction>
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="1:50 pm Client: Juan Items: 3water"  />
+          <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+        </ListItemSecondaryAction>
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="2:01 pm Client: Rocio Items: 1water"  />
+          <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+        </ListItemSecondaryAction>
+        </ListItem>
+      </List>
+    </Box>
   );
 }
 
